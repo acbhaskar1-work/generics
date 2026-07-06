9ca6f760a615645df8f7d11c5c3b25a46a0a80ff
acbhaskar1-work <acbhaskar1+work@gmail.com>
2026-07-06 23:04:19 +0530

    similarly what if it user object before understandig generics

--- src/com/chitkara/Main.java
+++ src/com/chitkara/Main.java
@@ -1,9 +1,15 @@
 package com.chitkara;
 
 import com.chitkara.generics.List;
+import com.chitkara.generics.User;
+import com.chitkara.generics.UserList;
 
 public class Main {
     public static void main(String[] args) {
+        var userList = new UserList();
+        var user = new User();
+        userList.add(user);
+        System.out.println(userList.get(0));
         var list = new List();
         list.add(1);
         list.add(2);
---
+++ src/com/chitkara/generics/UserList.java
@@ -0,0 +1,13 @@
+package com.chitkara.generics;
+
+public class UserList {
+    private User[] items = new User[10];
+    private int count;
+
+    public void add(User item){
+        items[count++] = item;
+    }
+    public User get(int index){
+        return items[index];
+    }
+}
