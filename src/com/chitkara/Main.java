ad8bbcbbecfe6f3dbbc87d6c2b57bbbae6aebf40
acbhaskar1 <acbhaskar1@gmail.com>
2026-07-06 18:55:21 +0530

    learning about generics to be understand before

---
+++ src/com/chitkara/Main.java
@@ -0,0 +1,13 @@
+package com.chitkara;
+
+import com.chitkara.generics.List;
+
+public class Main {
+    public static void main(String[] args) {
+        var list = new List();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        System.out.println(list.get(1));
+        }
+    }
