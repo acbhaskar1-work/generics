2f2d8ec9c5c1684a104accf34cb51a97cd3acab4
acbhaskar1-work <acbhaskar1+work@gmail.com>
2026-07-07 14:11:08 +0530

    The Exceptions

--- src/com/chitkara/Main.java
+++ src/com/chitkara/Main.java
@@ -1,19 +1,10 @@
+// Learning exceptions
 package com.chitkara;
 
-import com.chitkara.generics.List;
-import com.chitkara.generics.User;
-import com.chitkara.generics.UserList;
+import com.chitkara.exceptions.ExceptionsDemo;
 
 public class Main {
     public static void main(String[] args) {
-        var userList = new UserList();
-        var user = new User();
-        userList.add(user);
-        System.out.println(userList.get(0));
-        var list = new List();
-        list.add(1);
-        list.add(2);
-        list.add(3);
-        System.out.println(list.get(1));
-        }
+        ExceptionsDemo.show();
     }
+}
---
+++ src/com/chitkara/exceptions/ExceptionsDemo.java
@@ -0,0 +1,11 @@
+package com.chitkara.exceptions;
+
+public class ExceptionsDemo {
+    public static void show(){
+        logExcpetion(null);
+    }
+
+    public static void logExcpetion(String message){
+        System.out.println(message.toUpperCase());
+    }
+}
