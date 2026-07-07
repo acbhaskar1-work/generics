fa3d56a4458788127120de5810491717b03acec7
acbhaskar1-work <acbhaskar1+work@gmail.com>
2026-07-07 20:46:08 +0530

    we added exception feaures

--- src/com/chitkara/exceptions/ExceptionsDemo.java
+++ src/com/chitkara/exceptions/ExceptionsDemo.java
@@ -1,7 +1,17 @@
 package com.chitkara.exceptions;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+
 public class ExceptionsDemo {
     public static void show(){
+
+        try {
+            var reader = new FileReader("file.txt");
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+
         logExcpetion(null);
     }
 
