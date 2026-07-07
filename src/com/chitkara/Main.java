b59f27c94dfcec3f0e30e6da1295b9015602aff2
acbhaskar1-work <acbhaskar1+work@gmail.com>
2026-07-07 12:46:31 +0530

    understanding what are generics

--- src/com/chitkara/Main.java
+++ src/com/chitkara/Main.java
@@ -1,19 +1,8 @@
+// Solution to generics understanding
 package com.chitkara;
-
-import com.chitkara.generics.List;
-import com.chitkara.generics.User;
-import com.chitkara.generics.UserList;
 
 public class Main {
     public static void main(String[] args) {
-        var userList = new UserList();
-        var user = new User();
-        userList.add(user);
-        System.out.println(userList.get(0));
-        var list = new List();
-        list.add(1);
-        list.add(2);
-        list.add(3);
-        System.out.println(list.get(1));
+
         }
     }
