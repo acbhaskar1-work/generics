e0749252090d1769f97d94ad5ff1be9f45db2fca
acbhaskar1-work <acbhaskar1+work@gmail.com>
2026-07-08 12:42:24 +0530

    Api response achieved with generic and collection

--- src/com/chitkara/Main.java
+++ src/com/chitkara/Main.java
@@ -4,11 +4,18 @@ import com.chitkara.generics.ApiResponse;
 import com.chitkara.generics.GenericList;
 import com.chitkara.generics.User;
 
+import java.util.TreeMap;
+
 
 public class Main {
     public static void main(String[] args) {
         var names = new ApiResponse<String>();
         names.response(0,true,"Invoice");
         System.out.println(names.getData());
+        TreeMap<Boolean,String> info = new TreeMap<>();
+        info.put(true,"Order not processed");
+        ApiResponse<TreeMap> custemer = new ApiResponse<>();
+        custemer.response(1,false, info);
+        System.out.println(custemer.getData());
     }
 }
