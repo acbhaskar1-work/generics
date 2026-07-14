d80b78ac9b0a449591773ffe87c4243f93eb491f
acbhaskar1-work <acbhaskar1+work@gmail.com>
2026-07-14 16:47:12 +0530

    Collector processin

---
+++ src/com/chitkara/collectionprocessing/JavaStreams.java
@@ -0,0 +1,66 @@
+package com.chitkara.collectionprocessing;
+
+import java.lang.String;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.*;
+import java.util.*;
+import java.nio.file.*;
+import java.io.IOException;
+
+public class JavaStreams {
+    public static void main(String[] args) throws IOException {
+        // 1. Integer Stream
+        IntStream
+                .range(1, 10)
+                .forEach(System.out::print);
+        System.out.println();
+
+        // 2. Integer Stream with skip
+        IntStream
+                .range(1, 10)
+                .skip(5)
+                .forEach(x -> System.out.println(x));
+        System.out.println();
+
+        // 3. Integer Stream with sum
+        System.out.println(
+                IntStream
+                        .range(1, 5)
+                        .sum());
+        System.out.println();
+
+        // 4. Stream.of, sorted and findFirst
+        Stream.of("Ava", "Aneri", "Alberto")
+                .sorted()
+                .findFirst()
+                .ifPresent(System.out::println);
+
+        // 5. Stream from Array, sort, filter and print
+        String[] names = {"Al", "Ankit", "Kushal", "Brent", "Sarika", "amanda", "Hans", "Shivika", "Sarah"};
+        Arrays.stream(names)	// same as Stream.of(names)
+                .filter(x -> x.startsWith("S"))
+                .sorted()
+                .forEach(System.out::println);
+
+        // 6. average of squares of an int array
+        Arrays.stream(new int[] {2, 4, 6, 8, 10})
+                .map(x -> x * x)
+                .average()
+                .ifPresent(System.out::println);
+
+        // 7. Stream from List, filter and print
+        List<String> people = Arrays.asList("Al", "Ankit", "Brent", "Sarika", "amanda", "Hans", "Shivika", "Sarah");
+        people
+                .stream()
+                .map(String::toLowerCase)
+                .filter(x -> x.startsWith("a"))
+                .forEach(System.out::println);
+
+        Path path = Paths.get("..","..","data","data.txt");
+        String content = Files.readString(path);
+        System.out.println(content);
+
+
+    }
+}
